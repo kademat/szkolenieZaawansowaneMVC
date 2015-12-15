6a327f4284b83c19fa45c51c358c518a01bac105
Mateusz <kademat@gmail.com>
2015-12-15 13:41:26 +0100

    Połączenie buildera z fakerem

--- TestData/TestData/Program.cs
+++ TestData/TestData/Program.cs
@@ -11,20 +11,24 @@ namespace TestData
     {
         static void Main(string[] args)
         {
-            //var users = Builder<User>.CreateListOfSize(10).Build();
+            var users = Builder<User>.CreateListOfSize(10)
+                .All()
+                .With(u => u.Name = Faker.NameFaker.Name())
+                .With(u => u.Email = Faker.InternetFaker.Email())
+                .Build();
 
-            //foreach (var user in users)
-            //{
-            //    Console.WriteLine("{0} {1}", user.Name, user.Email);
-            //}
-
-            var user = new User()
+            foreach (var user in users)
             {
-                Name = Faker.NameFaker.FirstName(),
-                Email = Faker.InternetFaker.Email()
-            };
+                Console.WriteLine("{0} {1}", user.Name, user.Email);
+            }
 
-            Console.WriteLine("{0} {1}", user.Name, user.Email);
+            //var user = new User()
+            //{
+            //    Name = Faker.NameFaker.FirstName(),
+            //    Email = Faker.InternetFaker.Email()
+            //};
+
+            //Console.WriteLine("{0} {1}", user.Name, user.Email);
 
             Console.ReadLine();
         }
