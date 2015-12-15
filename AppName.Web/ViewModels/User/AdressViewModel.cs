2ca0f1005a8a2bbbebc90a42abceb3cbde980c92
Mateusz <kademat@gmail.com>
2015-12-15 14:13:18 +0100

    Dodawanie viewmodeli do projektu

---
+++ AppName.Web/ViewModels/User/AdressViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AppName.Web.ViewModels.User
+{
+    public class AdressViewModel
+    {
+        public AdressViewModel()
+        {
+            Adress = new AdressViewModel();
+        }
+        public string City { get; set; }
+
+        public AdressViewModel Adress { get; set; }
+    }
+}
---
+++ AppName.Web/ViewModels/User/CreateVIewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AppName.Web.ViewModels.User
+{
+    public class CreateViewModel
+    {
+        public string Name { get; set; }
+
+    }
+}
