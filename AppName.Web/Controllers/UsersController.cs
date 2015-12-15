b5ad5b1c655817b9fe5a94453aa129951ac5f67f
Mateusz <kademat@gmail.com>
2015-12-15 14:22:22 +0100

    Dodanie widoków - pokazanie błędu z partial views

---
+++ AppName.Web/Controllers/UsersController.cs
@@ -0,0 +1,29 @@
+﻿using AppName.Web.ViewModels.User;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AppName.Web.Controllers
+{
+    public class UsersController : Controller
+    {
+        // GET: Users
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            return View(new CreateViewModel());
+        }
+
+        [HttpPost]
+        public ActionResult Create(CreateViewModel viewModel)
+        {
+            return View(viewModel);
+        }
+    }
+}
