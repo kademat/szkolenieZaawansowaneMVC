bb8d7e94362a915be7926d2e984646a394f5912a
Mateusz <kademat@gmail.com>
2015-12-15 12:54:00 +0100

    Użycie convertera

--- AutomapperTest/AutomapperTest/Program.cs
+++ AutomapperTest/AutomapperTest/Program.cs
@@ -11,14 +11,15 @@ namespace AutomapperTest
     {
         static void Main(string[] args)
         {
+            Mapper.CreateMap<Enum, string>()
+                .ConvertUsing<EnumToStringConverter>();
+
             Mapper.CreateMap<Order, OrderDTO>()
                 .ForMember(dest => dest.Cost,
                 opt => opt.MapFrom(src => string.Format("{0} zl", src.Cost)))
-                .ForMember(dest => dest.Status,
-                    opt => opt.MapFrom(src =>
-
-
-                      src.Status.ToDisplayString()))
+                //.ForMember(dest => dest.Status,
+                //    opt => opt.MapFrom(src =>
+                //      src.Status.ToDisplayString()))
                 .IgnoreAllNonExisting();
 
             Mapper.AssertConfigurationIsValid();
