1c709ca0b2785a7e3bf604ee280f84ea74fff082
Mateusz <kademat@gmail.com>
2015-12-15 15:08:12 +0100

    Przykład użycia dla baseControler - generowanie nowego szablonu

---
+++ AppName.Web/Controllers/TestController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AppName.Web.Infrastructure;
+
+namespace AppName.Web.Controllers
+{
+	public class TestController : BaseController
+	{
+		//
+		// GET: /Test/
+		public ActionResult Index()
+		{
+			return View();
+		}
+	}
+}
