932407b686afcbc26e4fec6eabfcf5563b23fbbe
Mateusz <kademat@gmail.com>
2015-12-16 16:09:59 +0100

    Dodanie do Quartz'a działającej aplikacji

---
+++ QuartzNetTest/QuartzNetTest/PrintJob.cs
@@ -0,0 +1,24 @@
+﻿using Quartz;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuartzNetTest
+{
+    public class PrintJob : IJob
+    {
+        public void Execute(IJobExecutionContext context)
+        {
+            Console.WriteLine("Test: {0}", DateTime.Now);
+
+
+
+            //int a = 10;
+            //var value = string.Format("Test: {0}", a);
+            ////ta jest lepsza - nie ma 2 razy rzutowania
+            //var value2 = string.Format("Test: {0}", a.ToString());
+        }
+    }
+}
--- QuartzNetTest/QuartzNetTest/Program.cs
+++ QuartzNetTest/QuartzNetTest/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Quartz;
+using Quartz.Impl;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +12,21 @@ namespace QuartzNetTest
     {
         static void Main(string[] args)
         {
+            IScheduler scheduler = StdSchedulerFactory.GetDefaultScheduler();
+
+            scheduler.Start();
+
+            IJobDetail job = JobBuilder.Create<PrintJob>().Build();
+
+            ITrigger trigger = TriggerBuilder.Create().StartNow().WithSimpleSchedule(x => x.WithIntervalInSeconds(1).RepeatForever()).Build();
+
+            scheduler.ScheduleJob(job, trigger);
+
+            Console.ReadLine();
+
+            scheduler.Shutdown();
+
+
         }
     }
 }
