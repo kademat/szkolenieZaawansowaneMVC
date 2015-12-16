fab64624c47c946fad2fae3eb12654a4968df1bb
Mateusz <kademat@gmail.com>
2015-12-16 08:57:37 +0100

    DOdanie klasy rozszerzającej metodę IRuleBuilderOptions aby ustawić nowy walidator

---
+++ FluentValidationTest/FluentValidationTest/IRuleBuilderOptionsExtensions.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FluentValidationTest
+{
+    public static class IRuleBuilderOptionsExtensions
+    {
+        public static IRuleBuilderOptions<T, TProperty> Nip<T, TProperty>(this IRuleBuilderOptions<T, TProperty> ruleBuilder)
+        {
+            return ruleBuilder.SetValidator(new NipValidator());
+        }
+    }
+}
